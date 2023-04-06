c1ae48940fa4666f3f1e4da26ca1e625d37690d4
BrandonKenny <bmkenny@stu.naperville203.org>
2023-04-06 11:50:36 -0500

    animation 2

--- Final Game/Assets/PlayerScript.cs
+++ Final Game/Assets/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public Animator animator;
     public int playerFlipped = 0;
     
     // Start is called before the first frame update
@@ -20,37 +21,45 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
         {
             velocity.y = 0;
+            animator.SetInteger("NormanState", 0);
         }
         else if (Input.GetKey(KeyCode.W))
         {
             velocity.y = 8;
+            animator.SetInteger("NormanState", 3);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             velocity.y = -8;
+            animator.SetInteger("NormanState", 4);
         }
         else
         {
             velocity.y = 0;
+            animator.SetInteger("NormanState", 0);
         }
 
         if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
         {
             velocity.x = 0;
+            animator.SetInteger("NormanState", 0);
         }
         else if (Input.GetKey(KeyCode.A))
         {
             velocity.x = -8;
             playerFlipped = 1;
+            animator.SetInteger("NormanState", 2);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             velocity.x = 8;
             playerFlipped = 0;
+            animator.SetInteger("NormanState", 1);
         }
         else
         {
             velocity.x = 0;
+            
         }
         
         this.gameObject.GetComponent<Rigidbody2D>().velocity = velocity;
